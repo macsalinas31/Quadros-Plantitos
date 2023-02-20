a7bfe2984a288991a011e76c846cab390d10c48a
James Dhenvir <118252954+jdhenvir@users.noreply.github.com>
2023-02-20 08:32:02 +0800

    Update axios.js

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export default axios.create({
-    baseURL: 'http://localhost/q-plantitos-laravel/api',
+    baseURL: 'https://dhen.tech/api',
     withCredentials: false,
 
 });
