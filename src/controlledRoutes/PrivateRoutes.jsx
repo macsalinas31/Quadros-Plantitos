77fa687ddbb77093146e730fbe5185798208ce0e
James Dhenvir <118252954+jdhenvir@users.noreply.github.com>
2023-02-21 12:26:46 +0800

    can add product

--- src/controlledRoutes/PrivateRoutes.jsx
+++ src/controlledRoutes/PrivateRoutes.jsx
@@ -3,8 +3,6 @@ import { useLocation, Navigate, Outlet } from "react-router-dom";
 const PrivateRoutes = ({allowedRoles}) => {
     const location = useLocation();
     const auth = JSON.parse(localStorage.getItem('auth'));
-    console.log(auth?.role)
-
     return(
         auth.role == 0
             ? <Outlet/>
